9afe7417c5f4e0980b7b2c176d9b1dee58b6987b
Abhishek Kumar <abhishekkumar958830@gmail.com>
2024-10-17 16:35:13 +0530

    mobile responsive

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,7 +2,7 @@ import AuthComponent from "@/components/AuthComponent";
 
 function page() {
   return (
-    <div className="bg-[url('/images/gradient-light.png')] bg-no-repeat bg-[length:150%_150%]  bg-center h-screen">
+    <div className="bg-[url('/images/gradient-light.png')] px-10 bg-no-repeat bg-[length:150%_150%]  bg-center h-screen">
      <AuthComponent urlLink="/signup" urlText="Register Here" emailBtn="Continue with Email" googleBtn="Sign in with Google" />
     </div>
   )
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import AuthComponent from "@/components/AuthComponent";
 
 function page() {
   return (
-    <div className="bg-[url('/images/gradient-light.png')] bg-no-repeat bg-[length:150%_150%]  bg-center h-screen">
+    <div className="bg-[url('/images/gradient-light.png')] px-10 bg-no-repeat bg-[length:150%_150%]  bg-center h-screen">
      <AuthComponent urlLink="/signin" urlText="Login Here" emailBtn="Register with Email" googleBtn="Sign up with Google" />
     </div>
   )
