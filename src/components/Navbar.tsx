e48c2c93d8948b9c6f59c92081541a806382b5df
Abhishek Kumar <abhishekkumar958830@gmail.com>
2025-03-01 21:54:04 +0530

    Checking repo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
         <header className="w-full f bg-white bg-opacity-30 backdrop-blur-lg fixed ">
   <nav className="flex justify-between py-4 px-10">
-    <div className="text-gray-800 text-lg font-bold">AgileHunt</div>
+    <div className="text-gray-800 text-lg font-bold">Agile</div>
     <ul className="flex space-x-4">
     <Link href={"/"} className="text-gray-800 hover:text-gray-600 cursor-pointer">Home</Link>
       <Link href={"/signin"} className="text-gray-800 hover:text-gray-600 cursor-pointer">Sign In</Link>
